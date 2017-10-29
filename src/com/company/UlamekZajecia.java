d6111f5434273741a847750149319a0224c7c916
B4kus <szysz.szymon@gmail.com>
2017-10-29 14:50:09 +0100

    New class

---
+++ src/com/company/UlamekZajecia.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class UlamekZajecia {
+
+
+}
+
+class Ulamek
+{
+    int licznik;
+    int mianownik;
+
+
+    Ulamek(int licznik, int mianownik)
+    {
+        this.licznik = licznik;
+        this.mianownik = mianownik;
+
+
+
+    }
+
+
+}
