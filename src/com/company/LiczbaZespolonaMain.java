f7d240cb8d71e3f9ca487bb8ebcd9d308968e889
B4kus <szysz.szymon@gmail.com>
2017-11-07 14:27:12 +0100

    Add dodawanie

--- src/com/company/LiczbaZespolonaMain.java
+++ src/com/company/LiczbaZespolonaMain.java
@@ -5,6 +5,13 @@ public class LiczbaZespolonaMain {
 
     public static void main(String[] args) {
 
+        LiczbaZespolona obj, obj2;
+
+        obj = new LiczbaZespolona(2,5);
+        obj2 = new LiczbaZespolona(5,1);
+
+
+
 
 
     }
@@ -12,19 +19,37 @@ public class LiczbaZespolonaMain {
 
 
 class LiczbaZespolona{
-    int liczbaPierwsza;
-    int liczbaDruga;
+    int liczbaRzeczy;
+    int liczbaUro;
 
 
-    LiczbaZespolona(int liczbaPierwsza, int liczbaDruga){
-        this.liczbaPierwsza = liczbaPierwsza;
-        this.liczbaDruga = liczbaDruga;
+    LiczbaZespolona(int liczbaRzeczy, int liczbaUro){
+        this.liczbaRzeczy = liczbaRzeczy;
+        this.liczbaUro = liczbaUro;
+
+
+    }
+/*
+    LiczbaZespolona dodawanie(LiczbaZespolona obj2){
+        int wynikPierwszy, wynikDrugi;
+        LiczbaZespolona wynik = null;
+        wynik.liczbaRzeczy = this.liczbaRzeczy + obj2.liczbaRzeczy;
+        wynik.liczbaUro = this.liczbaUro + obj2.liczbaUro;
+
+        return wynik;
+
 
 
     }
 
+*/
+    void dodawanie(LiczbaZespolona obj2){
+        int wynikRzecz, wynikUro;
+        wynikRzecz = this.liczbaRzeczy + obj2.liczbaRzeczy;
+        wynikUro = this.liczbaUro + obj2.liczbaUro;
+        System.out.println("Nowa para liczb to: " + wynikRzecz + " " + wynikUro );
 
-
+    }
 
 
 
