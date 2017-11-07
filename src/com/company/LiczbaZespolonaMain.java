4790511615cec6af745422b5bfbc9afbc14bd075
B4kus <szysz.szymon@gmail.com>
2017-11-07 17:01:01 +0100

    Add dzielnie

--- src/com/company/LiczbaZespolonaMain.java
+++ src/com/company/LiczbaZespolonaMain.java
@@ -12,6 +12,7 @@ public class LiczbaZespolonaMain {
         obj.dodawanie(obj2);
         obj.odejmownaie(obj2);
         obj.mnozenie(obj2);
+        obj.dzielnie(obj2);
 
     }
 }
@@ -46,14 +47,14 @@ class LiczbaZespolona{
         int wynikRzecz, wynikUro;
         wynikRzecz = this.liczbaRzeczy + obj2.liczbaRzeczy;
         wynikUro = this.liczbaUro + obj2.liczbaUro;
-        System.out.println("Nowa para liczb to: " + wynikRzecz + " " + wynikUro );
+        System.out.println("Nowa para liczb to: " + wynikRzecz + " + " + wynikUro + "i");
 
     }
     void odejmownaie(LiczbaZespolona obj2){
         int wynikRecz, wynikUro;
         wynikRecz = this.liczbaRzeczy - obj2.liczbaRzeczy;
         wynikUro = this.liczbaUro = obj2.liczbaUro;
-        System.out.println("Nowa para liczb to: " + wynikRecz + " " + wynikUro );
+        System.out.println("Nowa para liczb to: " + wynikRecz + " - " + wynikUro + "i");
 
     }
 
@@ -61,10 +62,24 @@ class LiczbaZespolona{
         int wynikRzecz, wynikUro;
         wynikRzecz = this.liczbaRzeczy * obj2.liczbaRzeczy - this.liczbaUro * obj2.liczbaUro;
         wynikUro = this.liczbaRzeczy * obj2.liczbaUro + this.liczbaUro * obj2.liczbaRzeczy;
-        System.out.println("Nowa para liczb to: " + wynikRzecz + " " + wynikUro );
+        System.out.println("Nowa para liczb to: " + wynikRzecz + " * " + wynikUro + "i" );
 
 
 
+    }
+
+    void dzielnie(LiczbaZespolona obj2){
+        int wynikRzecz, wynikUro, wynikUlamka;
+        if (obj2.liczbaRzeczy == 0  && obj2.liczbaUro == 0){
+            System.out.println("Ups blad, nie dzielimy przez zero");
+        }else{
+           wynikRzecz = this.liczbaRzeczy * obj2.liczbaUro + this.liczbaUro * obj2.liczbaRzeczy;
+           wynikUro = this.liczbaUro * obj2.liczbaRzeczy - this.liczbaRzeczy * obj2.liczbaUro;
+           wynikUlamka = obj2.liczbaRzeczy * obj2.liczbaRzeczy + obj2.liczbaUro + obj2.liczbaUro;
+           System.out.println("Wynik dzielenia: " + wynikRzecz + "/" + wynikUlamka + " + "+ wynikUro + "/" + wynikUlamka + "i");
+
+
+        }
     }
 
 
