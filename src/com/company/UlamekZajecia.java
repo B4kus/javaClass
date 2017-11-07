158f9bedd4e55f81bb7b9737d3e48b5473304a09
B4kus <szysz.szymon@gmail.com>
2017-11-07 12:45:06 +0100

    Add nwd

--- src/com/company/UlamekZajecia.java
+++ src/com/company/UlamekZajecia.java
@@ -6,7 +6,7 @@ public class UlamekZajecia {
     {
         Ulamek obj, obj2, obj3;
 
-        obj = new Ulamek(8,5);
+        obj = new Ulamek(8,4);
         obj2 = new Ulamek(3,7);
         obj3 = new Ulamek(3,2);
         obj.dodawanie(obj2);
@@ -14,45 +14,47 @@ public class UlamekZajecia {
         obj.mnozenie(obj2);
         obj.odwroc();
 
+
         double wynikDziesietny = obj.rozDziesietne();
         System.out.println("Rozwiniecie dziesietne ulamka to: " + wynikDziesietny);
+
 
 
     }
 }
 
-class Ulamek
-{
+class Ulamek {
     int licznik;
     int mianownik;
 
 
-    Ulamek(int licznik, int mianownik)
-    {
+    Ulamek(int licznik, int mianownik) {
         this.licznik = licznik;
         this.mianownik = mianownik;
 
 
     }
-    void dodawanie(Ulamek obj2){
+
+    void dodawanie(Ulamek obj2) {
         int wynikLicznik, wynikMianownik;
-        if (this.mianownik == obj2.mianownik){
-           wynikLicznik = this.licznik + obj2.licznik;
-           wynikMianownik = this.mianownik;
-           System.out.println("Wynik dodawania ułamków to:" + " " +  wynikLicznik + " " + wynikMianownik);
-        }else{
+        if (this.mianownik == obj2.mianownik) {
+            wynikLicznik = this.licznik + obj2.licznik;
+            wynikMianownik = this.mianownik;
+            System.out.println("Wynik dodawania ułamków to:" + " " + wynikLicznik + " " + wynikMianownik);
+        } else {
             wynikLicznik = (this.licznik * obj2.mianownik) + (this.mianownik * obj2.licznik);
             wynikMianownik = this.mianownik * obj2.mianownik;
             System.out.println("Wynik dodawania ułamków to:" + " " + wynikLicznik + " " + wynikMianownik);
         }
     }
-    void odejmowanie(Ulamek obj2){
+
+    void odejmowanie(Ulamek obj2) {
         int wynikLicznika, wynikMianownika;
-        if (this.mianownik == obj2.mianownik){
+        if (this.mianownik == obj2.mianownik) {
             wynikLicznika = this.licznik - obj2.licznik;
             wynikMianownika = this.mianownik;
-            System.out.println("Wynik dodawania ułamków to:" + " " +  wynikLicznika + " " + wynikMianownika);
-        }else{
+            System.out.println("Wynik dodawania ułamków to:" + " " + wynikLicznika + " " + wynikMianownika);
+        } else {
             wynikLicznika = (this.licznik * obj2.mianownik) - (this.mianownik * obj2.licznik);
             wynikMianownika = this.mianownik * obj2.mianownik;
             System.out.println("Wynik dodawania ułamków to:" + " " + wynikLicznika + " " + wynikMianownika);
@@ -61,15 +63,15 @@ class Ulamek
 
     }
 
-    void mnozenie(Ulamek obj2){
+    void mnozenie(Ulamek obj2) {
         int wynikLicznika, wynikMianownika;
         wynikLicznika = this.licznik * obj2.licznik;
         wynikMianownika = this.mianownik * obj2.mianownik;
-        System.out.println("Wynik mnożenia ułamków to:" + " " +  wynikLicznika + " " + wynikMianownika);
+        System.out.println("Wynik mnożenia ułamków to:" + " " + wynikLicznika + " " + wynikMianownika);
 
     }
 
-    double rozDziesietne(){
+    double rozDziesietne() {
         double wynik;
         double nowyMianownik = (double) mianownik;
         wynik = licznik / nowyMianownik;
@@ -77,19 +79,33 @@ class Ulamek
         return wynik;
 
 
+    }
+
+    void odwroc() {
+
+        int odwLicznik, odwMianownik;
+        odwLicznik = mianownik;
+        odwMianownik = licznik;
+        System.out.println("Odwrotnosc ulamka to:" + " " + odwLicznik + " " + odwMianownik);
 
 
     }
-    void odwroc(){
 
-      int odwLicznik, odwMianownik;
-      odwLicznik = mianownik;
-      odwMianownik = licznik;
-      System.out.println("Odwrotnosc ulamka to:" + " " + odwLicznik + " "+ odwMianownik);
+    int nwd() {
 
+        while (licznik != mianownik) {
+            if (licznik > mianownik) {
 
+                licznik -= mianownik;
+
+            } else {
+
+                mianownik -= licznik;
+
+            }
+        }
+        return licznik;
     }
-
 
 
 }
