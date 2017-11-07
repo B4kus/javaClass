cd83c071a8abb5968e8d9aba979d0fa942bea1ca
B4kus <szysz.szymon@gmail.com>
2017-11-07 15:48:22 +0100

    Add wektor, inicjalizacja

---
+++ src/com/company/WektorMain.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class WektorMain {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
+class Wektor{
+    int wektorX;
+    int wektorY;
+    int wektorZ;
+
+    Wektor(int wektorX, int wektorY, int wektorZ){
+
+        this.wektorX = wektorX;
+        this.wektorY = wektorY;
+        this.wektorZ = wektorZ;
+
+
+    }
+
+
+}
