c4f7b494cbe73504054ea13b8199b148647c0942
B4kus <szysz.szymon@gmail.com>
2017-11-07 17:33:31 +0100

    Add inicjalizacja

---
+++ src/com/company/BigLiczbaMain.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class BigLiczbaMain {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
+
+
+class BigLiczba{
+
+    int liczba;
+
+
+    BigLiczba(int liczba){
+        this.liczba = liczba;
+
+    }
+
+
+
+
+
+}
