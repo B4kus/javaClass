3713d3b1f7a94f7db1260b2bf4efa46a260687cf
B4kus <szysz.szymon@gmail.com>
2017-11-09 14:46:36 +0100

    Add mnozenie

--- src/com/company/WielomianKwadratowy.java
+++ src/com/company/WielomianKwadratowy.java
@@ -5,9 +5,10 @@ public class WielomianKwadratowy {
     public static void main(String[] args) {
 
         WieloKwadratowy obj, obj2;
-        obj = new WieloKwadratowy(3,5,9);
-        obj2 = new WieloKwadratowy(5,1,7);
+        obj = new WieloKwadratowy(3,5,3);
+        obj2 = new WieloKwadratowy(5,1,2);
         obj.dodawanie(obj2);
+        obj.mnozenie(obj2);
 
     }
 
@@ -19,10 +20,12 @@ class WieloKwadratowy {
     int b;
     int c;
 
+
     WieloKwadratowy(int a, int b, int c){
             this.a = a;
             this.b = b;
             this.c = c;
+
 
 
     }
@@ -35,7 +38,11 @@ class WieloKwadratowy {
         wynikC = this.c + obj2.c;
         System.out.println("Wynik dodawanie dwoch wielomianow to: " + wynikA + "x*x" + " + " + wynikB + "x" + " + "+ wynikC);
 
+    }
 
+    void mnozenie(WieloKwadratowy obj2){
+        System.out.println("Wynik mnozenia dwoch wielomianow to: " + this.a * obj2.a + "x*x*x*x" + " + " + ((this.a * obj2.b) + (this.b * obj2.a)) + "x*x*x" + " + "+
+                ((this.a * obj2.c) + (this.b * obj2.b) + (this.c * obj2.a)) + "x*x" + " + " + (this.b * obj2.c + this.c * obj2.b) + " x " + " + " + (this.c * obj2.c ));
 
     }
 
