ba8c575d6837e8dae2b74c9fe83ac4c543de2188
B4kus <szysz.szymon@gmail.com>
2017-11-09 15:03:05 +0100

    Add miejsce zerowe

--- src/com/company/WielomianKwadratowy.java
+++ src/com/company/WielomianKwadratowy.java
@@ -5,10 +5,11 @@ public class WielomianKwadratowy {
     public static void main(String[] args) {
 
         WieloKwadratowy obj, obj2;
-        obj = new WieloKwadratowy(3,5,3);
+        obj = new WieloKwadratowy(-5,6,-2);
         obj2 = new WieloKwadratowy(5,1,2);
         obj.dodawanie(obj2);
         obj.mnozenie(obj2);
+        obj.miejsceZerwo();
 
     }
 
@@ -44,6 +45,22 @@ class WieloKwadratowy {
         System.out.println("Wynik mnozenia dwoch wielomianow to: " + this.a * obj2.a + "x*x*x*x" + " + " + ((this.a * obj2.b) + (this.b * obj2.a)) + "x*x*x" + " + "+
                 ((this.a * obj2.c) + (this.b * obj2.b) + (this.c * obj2.a)) + "x*x" + " + " + (this.b * obj2.c + this.c * obj2.b) + " x " + " + " + (this.c * obj2.c ));
 
+    }
+    void miejsceZerwo(){
+        int delta;
+        double miejsceJeden, miejsceDwa, miejsceZero;
+        delta = this.b * this.b - 4 * this.a * this.c;
+        System.out.println(delta);
+        if (delta > 0) {
+            miejsceJeden = (-this.b + Math.sqrt(delta)) / (2 * this.a);
+            miejsceDwa = (-this.b - Math.sqrt(delta)) / (2 * this.a);
+            System.out.println("Miejsca zerwo funkcji to: " + miejsceJeden + " oraz " + miejsceDwa);
+        }else if (delta == 0){
+            miejsceZero = - this.b/(2 * this.a);
+            System.out.println("Miejsce zero funkcji to: " + miejsceZero);
+        }else {
+            System.out.println("Brak miejsc zerowych funkcji.");
+        }
     }
 
 
